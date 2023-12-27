83356632be59990f0839eaf832689832a7293eae
Cooper Stasinowsky <cjstaz13@gmail.com>
2023-12-27 11:50:27 +1100

    create column function

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('#box');
 
-for (let i = 1; i <= 16; i++) {
-    const newDiv = document.createElement('div')
-    container.appendChild(newDiv);
-    newDiv.setAttribute('style', 'border: 1px solid black; width: 40px; height: 40px;');
+let column = function() {
+    for (let i = 1; i <= 16; i++) {
+        const newDiv = document.createElement('div')
+        container.appendChild(newDiv);
+        newDiv.setAttribute('style', 'border: 1px solid black; width: 40px; height: 40px;');
+    }
 }
