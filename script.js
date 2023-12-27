9359e273ac27d64434648d7ea21c01b937c1c77a
Cooper Stasinowsky <cjstaz13@gmail.com>
2023-12-27 12:22:02 +1100

    Need to use flexbox to wrap columns onto other lines

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 const container = document.querySelector('#box');
 
 let column = function() {
+        const column = document.createElement('div');
+        column.classList.add('column');
+        container.appendChild(column);
+    
     for (let i = 1; i <= 16; i++) {
-        const newDiv = document.createElement('div')
-        container.appendChild(newDiv);
+        const newDiv = document.createElement('div');
+        column.appendChild(newDiv);
         newDiv.setAttribute('style', 'border: 1px solid black; width: 40px; height: 40px;');
-    }
+    };
+};
+
+let square = function() {
+    for (let i = 1; i <= 16; i++) {
+        column();
+    };
 }
+
+square();
