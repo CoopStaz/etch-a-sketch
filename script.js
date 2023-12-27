b3f79275599dffa49385733037351a04103e7c67
Cooper Stasinowsky <cjstaz13@gmail.com>
2023-12-27 15:45:58 +1100

    Begin creation of hover effect

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const container = document.querySelector('#box');
+container.setAttribute('style', 'display: flex; justify-content: center;');
 
-let column = function() {
+const square = function() {
+    const column = function() {
         const column = document.createElement('div');
         column.classList.add('column');
         container.appendChild(column);
     
-    for (let i = 1; i <= 16; i++) {
-        const newDiv = document.createElement('div');
-        column.appendChild(newDiv);
-        newDiv.setAttribute('style', 'border: 1px solid black; width: 40px; height: 40px;');
+        for (let i = 1; i <= 16; i++) {
+            const newDiv = document.createElement('div');
+            column.appendChild(newDiv);
+            newDiv.setAttribute('style', 'border: 1px solid black; width: 40px; height: 40px;');
+        };
     };
-};
-
-let square = function() {
     for (let i = 1; i <= 16; i++) {
         column();
     };
-}
-
-container.setAttribute('style', 'display: flex');
+};
 
 square();
