0105472b7d64c550c638f7f21ec56033ba654884
Cooper Stasinowsky <cjstaz13@gmail.com>
2023-12-28 20:47:41 +1100

    Add blue hover over effect to grid

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 const container = document.querySelector('#box');
 container.setAttribute('style', 'display: flex; justify-content: center;');
 
-const square = function() {
-    const column = function() {
-        const column = document.createElement('div');
-        column.classList.add('column');
-        container.appendChild(column);
-    
-        for (let i = 1; i <= 16; i++) {
-            const newDiv = document.createElement('div');
-            column.appendChild(newDiv);
-            newDiv.setAttribute('style', 'border: 1px solid black; width: 40px; height: 40px;');
-        };
-    };
-    for (let i = 1; i <= 16; i++) {
-        column();
+for (let i = 1; i <= 16; i++) {
+    const column = document.createElement('div');
+    column.classList.add('column');
+    container.appendChild(column);
+        
+     for (let i = 1; i <= 16; i++) {
+        const newDiv = document.createElement('div');
+         newDiv.classList.add('square');
+         column.appendChild(newDiv);
+         newDiv.setAttribute('style', 'border: 1px solid black; width: 40px; height: 40px;');
+
+         newDiv.addEventListener('mouseover', function() {
+            newDiv.style.backgroundColor = 'blue';
+        });
     };
 };
-
-square();
-
-container.addEventListener('mouse-over', function() {
-    container.setAttribute('style,')
-});
