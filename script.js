478b3d2642e5415a750b0ac93f33e31f59483e5c
Cooper Stasinowsky <cjstaz13@gmail.com>
2023-12-29 20:03:25 +1100

    Add a button that grants the ability to change grid size.

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const container = document.querySelector('#box');
 container.setAttribute('style', 'display: flex; justify-content: center;');
 
 for (let i = 1; i <= 16; i++) {
-    const column = document.createElement('div');
+    let column = document.createElement('div');
     column.classList.add('column');
     container.appendChild(column);
         
      for (let i = 1; i <= 16; i++) {
-        const newDiv = document.createElement('div');
+        let newDiv = document.createElement('div');
          newDiv.classList.add('square');
          column.appendChild(newDiv);
          newDiv.setAttribute('style', 'border: 1px solid black; width: 40px; height: 40px;');
@@ -17,3 +17,37 @@ for (let i = 1; i <= 16; i++) {
         });
     };
 };
+
+//Click a button that will show a prompt
+//Ability to enter any number from 1 to 100
+//Delete previous grid
+//Create a grid using the chosen number
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', function() {
+    let numInput = prompt('What is your chosen grid width?');
+    if (numInput > 100) {
+        alert('ERROR \nThe number chosen cannot be larger than 100');
+    } else {
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
+        for (let i = 1; i <= numInput; i++) {
+            let column = document.createElement('div');
+            column.classList.add('column');
+            container.appendChild(column);
+                
+             for (let i = 1; i <= numInput; i++) {
+                let newDiv = document.createElement('div');
+                 newDiv.classList.add('square');
+                 column.appendChild(newDiv);
+                 newDiv.setAttribute('style', 'border: 1px solid black; width: 40px; height: 40px;');
+        
+                 newDiv.addEventListener('mouseover', function() {
+                    newDiv.style.backgroundColor = 'blue';
+                });
+            };
+        };
+    }
+});
